0da61b6a44403438ed8d9643c36542474b9c252c
Happy Situ <happysitu@utexas.edu>
2016-01-28 23:38:25 -0600

    PLProject1

---
+++ src/situ/customer/CustomerApp.java
@@ -0,0 +1,10 @@
+package situ.customer;
+
+public class CustomerApp {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
---
+++ src/situ/customer/CustomerDB.java
@@ -0,0 +1,10 @@
+package situ.customer;
+
+public class CustomerDB {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
